85e08e7fb192ef5b42e1ecf68c656d1c5920359b
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-05-30 19:01:41 +0530

    keep pushing

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,26 +1,36 @@
 
 import { useState, useEffect } from "react";
 import { copy , linkIcon, loader , logo } from "../assets";
+// import { useLazyGetSummaryQuery } from "../services/article";
+
 const Demo = () => {
     const[article,setArticle] = useState({
         url:'',
         summary: '',
-    });
+    })};
 
-    const handlSubmit = async(e) =>{
-        alert('submitted');
-    } 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        const existingArticle = allArticles.find(
+          (item) => item.url === article.url
+        );
+
+
     return (
         <section className="mt-16 w-full max-w-xl">
             <div className="flex flex-col w-full gap-2">
                 <form className=" relative justify-centre items-center" on onSubmit={handlSubmit}>
                     <img src="link_icon" className="absolute left-0 my-2 ml-3 w-5"/>
-                    <input type="url"
-                    placeholder="Enter a Url"
-                    value="article.url"
-                    onChange={(e) => setArticle({... article,url:e.target.value })}
-                    required className= "url_input peer"
-                    />
+                    <input
+            type='url'
+            placeholder='Paste the article link'
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            onKeyDown={handleKeyDown}
+            required
+            className='url_input peer'
+          />
                     <button
                     type= "submit"
                     className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"></button>
