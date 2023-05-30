ac73c35a532308a2a022963fd35aa1a31b243396
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-05-30 18:55:04 +0530

    pushing further

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
 
-import Hero from './components/Hero';
-import Demo from './components/Demo';
-import './App.css'
-const App = ()  => {
+import "./App.css";
+
+const App = () => {
   return (
     <main>
       <div className='main'>
-        <div className='gradient'/>
+        <div className='gradient' />
       </div>
       <div className='app'>
-        <Hero/>
-        <Demo/>
+        <Hero />
+        <Demo />
       </div>
     </main>
-  )
-}
+  );
+};
 
-
-export default App
+export default App;
---
+++ src/components/Demo.jsx
@@ -0,0 +1,39 @@
+
+import { useState, useEffect } from "react";
+import { copy , linkIcon, loader , logo } from "../assets";
+const Demo = () => {
+    const[article,setArticle] = useState({
+        url:'',
+        summary: '',
+    });
+
+    const handlSubmit = async(e) =>{
+        alert('submitted');
+    } 
+    return (
+        <section className="mt-16 w-full max-w-xl">
+            <div className="flex flex-col w-full gap-2">
+                <form className=" relative justify-centre items-center" on onSubmit={handlSubmit}>
+                    <img src="link_icon" className="absolute left-0 my-2 ml-3 w-5"/>
+                    <input type="url"
+                    placeholder="Enter a Url"
+                    value="article.url"
+                    onChange={(e) => setArticle({... article,url:e.target.value })}
+                    required className= "url_input peer"
+                    />
+                    <button
+                    type= "submit"
+                    className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"></button>
+                </form>
+
+                {/* Browse URl History*/}
+               
+
+
+            </div>
+
+        </section>
+
+    )
+}
+export default Demo
