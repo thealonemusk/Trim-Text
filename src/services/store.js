874f3dfe2daf1aa83b58834e9a5da832231d6deb
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-06-01 20:03:59 +0530

    fixed article and store

--- src/services/store.js
+++ src/services/store.js
@@ -1,11 +1,10 @@
-import { configureStore, getDefaultMiddleware
- } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
- import { articleApi } from "./article";
+import { articleApi } from "./article";
 
- export const store = configureStore ({
-    reducer:{
-        [articleApi.reducerPath]: articleApi.reducer
+export const store = configureStore({
+    reducer: {
+        [articleApi.reducerPath]: articleApi.reducer,
     },
-    middleware:(getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
- });
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
+})
