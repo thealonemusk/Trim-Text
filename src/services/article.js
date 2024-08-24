3ab8eff5448f6087cd094d851dbc34d7f7419e4b
Ashutosh Jha <105535224+thealonemusk@users.noreply.github.com>
2024-08-24 18:02:36 +0530

    updated the backed code, this is for making the backend run onthe render

--- src/services/article.js
+++ src/services/article.js
@@ -24,3 +24,33 @@ export const articleApi = createApi({
 });
 
 export const { useLazyGetSummaryQuery } = articleApi;
+
+
+
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// // Assuming your FastAPI server is running on localhost at port 8000
+// const baseUrl = "https://api-jmwm.onrender.com";
+
+// export const articleApi = createApi({
+//   reducerPath: "articleApi",
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: baseUrl,
+//     prepareHeaders: (headers) => {
+//       // Setting Content-Type to application/x-www-form-urlencoded since FastAPI is expecting form data
+//       headers.set("Content-Type", "application/x-www-form-urlencoded");
+//       return headers;
+//     },
+//   }),
+//   endpoints: (builder) => ({
+//     getSummary: builder.query({
+//       query: (params) => ({
+//         url: `/summarize/`,
+//         method: 'POST',
+//         body: new URLSearchParams({ url: params.articleUrl }).toString(), // Sending the URL as form data
+//       }),
+//     }),
+//   }),
+// });
+
+// export const { useLazyGetSummaryQuery } = articleApi;
